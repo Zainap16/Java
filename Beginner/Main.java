fb0a0db50599cbb9ad733ef31ceb700b4328cbaf
Zainap16 <zvanblerck1@gmail.com>
2023-10-24 20:33:31 +0200

    Pythagorean Theorem

--- Beginner/Main.java
+++ Beginner/Main.java
@@ -1,7 +1,22 @@
 package Beginner;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Print this messsage");
+        double inputA, inputB = 0;
+        System.out.println("Thsi will calculate the hypotenuse");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Type a number: ");
+        inputA = scanner.nextDouble();
+
+        System.out.println("Type a number: ");
+        inputB = scanner.nextDouble();
+
+        double total = Math.sqrt(Math.pow(inputA, 2) + Math.pow(inputB, 2));
+
+        System.out.println(total);
+
     }
 }
